65e0876ae36fae1924c512094336020a770f7db5
mertcancet <mertcancet95@gmail.com>
2021-05-19 15:59:26 +0300

    Add embedded timeline

--- components/Timeline.js
+++ components/Timeline.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import SearchBox from './SearchBox';
-
+import { TwitterTimelineEmbed } from 'react-twitter-embed';
 const Timeline = () => {
   return (
     <div className=' w-[355px]'>
       <SearchBox />
+      <div className='m-3 h-[100vh-60px]'>
+        <TwitterTimelineEmbed
+          sourceType='profile'
+          screenName='reactjs'
+          theme='dark'
+          options={{ height: '800' }}
+        />
+      </div>
     </div>
   );
 };
